9267e9797fa8a5c4d9ed47a726e7de87760f6c21
Dina Kazakova <dina1kazakova@yandex.ru>
2024-09-08 13:35:02 +0300

    версия 1

---
+++ src/components/view/Modal.ts
@@ -0,0 +1,34 @@
+import { IEvents } from '../base/Events';
+import { ensureElement } from '../../utils/utils';
+import { Component } from '../base/Component';
+
+export interface IModal {
+	content: HTMLElement;
+}
+
+export class Modal extends Component<IModal> {
+	protected _closeButton: HTMLButtonElement;
+	protected _content: HTMLElement;
+
+	constructor(container: HTMLElement, protected events: IEvents) {
+		super(container);
+		this._content = ensureElement<HTMLElement>(
+			'.modal__content',
+			this.container
+		);
+		this._closeButton = ensureElement<HTMLButtonElement>(
+			'.close__button',
+			this.container
+		);
+
+		this._closeButton.addEventListener('click', this.closeModal.bind(this));
+	}
+	closeModal() {
+		this.toggleClass(this.container, 'modal_active', false);
+		this.events.emit('modal:close');
+	}
+	openModal() {
+		this.toggleClass(this.container, 'modal_active', true);
+		this.events.emit('modal:open');
+	}
+}
---
+++ src/components/view/Page.ts
@@ -0,0 +1,23 @@
+import { Component } from '../base/Component';
+import { IEvents } from '../base/Events';
+import { ensureElement } from '../../utils/utils';
+import { IPage } from '../../types';
+
+export class Page extends Component<IPage> {
+	protected _catalog: HTMLElement;
+	protected _basket: HTMLElement;
+	protected _counter: HTMLElement;
+	protected _wrapper: HTMLElement;
+
+	constructor(container: HTMLElement, protected events: IEvents) {
+		super(container);
+		this._catalog = ensureElement<HTMLElement>('.gallery');
+		this._basket = ensureElement<HTMLElement>('.header__basket');
+		this._counter = ensureElement<HTMLElement>('.header__basket-counter');
+		this._wrapper = ensureElement<HTMLElement>('.page__wrapper');
+	}
+
+	set catalog(items: HTMLElement[]) {
+		this._catalog.replaceChildren(...items);
+	}
+}
